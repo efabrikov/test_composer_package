20f7dc1689786ebc87ec56f32aa8de4fd4c18ef8
Жека <@192.168.1.4>
2015-02-02 21:27:14 +0200

    pac 2

---
+++ src/Novanova/Helpers/IP2.php
@@ -0,0 +1,14 @@
+<?php
+namespace Novanova\Helpers;
+
+/**
+ * Description of IP2
+ *
+ * @author Eugene Fabrikov <eugene.fabrikov@gmail.com>
+ */
+class IP2
+{
+    public static function getIP2() {
+        echo 'IP2';
+    }
+}
