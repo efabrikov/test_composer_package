e5822dd421d30cf3596076752efd2c95c9fa11c6
Жека <@192.168.1.4>
2015-02-02 22:01:26 +0200

    pac 2

---
+++ src/Test0.php
@@ -0,0 +1,14 @@
+<?php
+namespace src;
+/**
+ * Description of Test1
+ *
+ * @author Eugene Fabrikov <eugene.fabrikov@gmail.com>
+ */
+class Test0
+{
+    public static function test()
+    {
+        echo 'test test0';
+    }
+}
