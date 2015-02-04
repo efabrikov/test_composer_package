7acb203cae2a714705aedea01f31228e886080c5
Жека <@192.168.1.4>
2015-02-04 11:42:08 +0200

    pac 2

---
+++ src/efabrikov/cms/admin/Test.php
@@ -0,0 +1,14 @@
+<?php
+namespace efabrikov\cms\admin;
+/**
+ * Description of Test
+ *
+ * @author Eugene Fabrikov <eugene.fabrikov@gmail.com>
+ */
+class Test
+{
+    public static function test()
+    {
+        echo 'test test from admin';
+    }
+}
